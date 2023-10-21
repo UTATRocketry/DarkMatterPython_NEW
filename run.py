52dbcc97f9f38dbb1150d8b89d95efb142808a3e
Eric <ericliu562@gmail.com>
2023-10-21 12:54:21 -0400

    New run file that instantiates rocket and incorportated simulation commands. Display will be added after the layering is done

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
-print(f"hello")
+import RocketSimulationClassFiles as rocket
+
+rocketVersion = 'houbolt_jr_single'
+instance1 = rocket.SimulationClass1(rocketVersion)
+instance1.simulate()
+print("Complete")
+
+#from RocketSimulationClassFiles.showOutputMenu import createAllPlots, showOutputMenu
