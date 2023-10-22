b798ab54e85ec49972ec2bb2da23c224b6a972d7
Eric <ericliu562@gmail.com>
2023-10-21 21:48:09 -0400

    New structure for retrieving inputs new folder created for the structure

--- RocketSimulationClassFiles/SimulationClass1.py
+++ RocketSimulationClassFiles/SimulationClass1.py
@@ -1,6 +1,23 @@
+import numpy as np
+import RocketSimulationClassFiles as rocket
+from RocketSimulationClassFiles import rocketVersionFiles
+
+import rocketVersionFiles
+
+
 class SimulationClass1:
-    def __init__(self, input_selector):
-        if not input_selector:
-            raise Exception('ERROR: simulationClass constructor executed without input arguments')
+    def __init__(self, selectedInput):
+        if not selectedInput:
+            raise Exception('ERROR: no rocket input selected')
         else:
-            rocket = input_selector
+            print()
+            rocket = self.getRocketSpecs(selectedInput)
+
+    def getRocketSpecs(self, input_selector):
+        if input_selector == "houbolt_jr_single":
+            ####rocketInput.yaml file might need to be changed#### (IF CHANGING TO 3D)
+            return rocketVersionFiles.houbolt_jr_single(
+                'RocketSimulationClassFiles/rocketVersionFiles/rocketInput.yaml')
+        # elif input_selector == ""
+        else:
+            raise Exception('Invalid rocket input')
