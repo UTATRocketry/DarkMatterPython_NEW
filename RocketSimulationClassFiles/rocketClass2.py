9c8378f3f0bbcab89c9c12ce4e342ee613d44b05
eliu <eric7liu562@gmail.com>
2023-10-21 22:05:09 -0400

    level 2 class structure (behind simulationClass)

---
+++ RocketSimulationClassFiles/rocketClass2.py
@@ -0,0 +1,6 @@
+import RocketSimulationClassFiles as rocket
+import numpy as np
+
+class rocketClass2:
+    def __init__(self):
+        print("Building Rocket Class")
---
+++ RocketSimulationClassFiles/utilityClass2.py
@@ -0,0 +1,5 @@
+import DMpkg as rocket
+
+class utilityClass2:
+    def __init__(self):
+        print("Building Utility Class")
