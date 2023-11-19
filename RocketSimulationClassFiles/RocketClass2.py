d50c38d19042b7f4fbc82b60c53383ac16339b3c
eliu <eric7liu562@gmail.com>
2023-11-19 09:35:48 -0500

    delete duplicate error handling and add connection to propulsion class

--- RocketSimulationClassFiles/RocketClass2.py
+++ RocketSimulationClassFiles/RocketClass2.py
@@ -2,8 +2,14 @@ import RocketSimulationClassFiles as rocket
 import numpy as np
 
 class RocketClass2:
-    def __init__(self):
+    def __init__(self, input):
+        #no need to check input is there, already checked in upper levels
         print("Building Rocket Class")
+        self.input = input
+        self.propulsion = rocket.PropulsionClass3(input)
+
+        #add airfram and utilities later
+
     def get_flight_type(self):
         #   Settings' is attribute storing a dict-like object : 'flightType'
         #   Add later
