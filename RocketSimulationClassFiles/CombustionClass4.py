6458e019a7dc7b6c6d8128834d5d880f00a5d81c
ericliu4 <eric7liu562@gmail.com>
2024-03-31 02:12:21 -0400

    complete combustion class - copy over get Cea

--- RocketSimulationClassFiles/CombustionClass4.py
+++ RocketSimulationClassFiles/CombustionClass4.py
@@ -32,4 +32,34 @@ class CombustionClass4:
         self.fuelTag = self.input['fuel']
 
 
-
+#No changes needed for 6DoF
+        # -----------------------------------------------------------------------
+        #   METHOD: get_CEA
+        #   Creates CEA object and calculates CEA outputs determines
+        #
+        #   INPUTS \..........................................................
+        #     - <OF> (numeric):     Oxidizer fuel ratio
+        #     - <Pcc> (numeric):    Combustion chamber pressure
+        #     - <exp> (numeric):    Nozzle expansion area ratio
+        #   OUTPUTS ............................................................
+        #     - <self> (class):    Returns combustionClass with exit gamma,
+        #                           mach number, specific impulse,
+        #                           exit temperature and pressure, chamber gamma,
+        #                           cstar, and chamber temperature
+        # -----------------------------------------------------------------------
+    def get_CEA(self, OF, Pcc, exp):
+        if exp == None:
+            exp = 3.887
+        houbolt_jr = Rocket.CEA(Pcc=Pcc, OF=OF, area_ratio=exp, Pamb=14.7,
+                                    oxName=self.input["ox"]["name"], fuelName=self.input["fuel"]["name"])
+        # area ratio is throat area / nozzle area (from nozzleClass)
+        self.output = Rocket.PropertyClass2()
+        self.output.gamm_e = houbolt_jr.exit_MolWt_gamma  # return the tuple (mw, gam) for the nozzle exit (lbm/lbmole, -)
+        self.output.rho_e = houbolt_jr.MachNumber
+        self.output.Isp = houbolt_jr.Isp
+        self.output.Te = houbolt_jr.T_e
+        self.output.Pe = houbolt_jr.P_e
+        self.output.Re = self.output.Pe / (self.output.rho_e * self.output.Te)
+        self.output.gamm = houbolt_jr.Chamber_MolWt_gamma  # return the tuple (mw, gam) for the chamber (lbm/lbmole, -)
+        self.output.cstar = houbolt_jr.Cstar
+        self.output.Tcc = houbolt_jr.Temperatures[0]
